2bad4c26a8e71e5fa457cd0bdf7d736e1f2a9520
Ziad Ak <ziadakgit@gmail.com>
2021-06-15 22:17:09 +0300

    update token

--- src/main/java/Help.java
+++ src/main/java/Help.java
@@ -6,19 +6,23 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
 public class Help extends ListenerAdapter {
+    String prefix = "!";
+
     @Override
     public void onPrivateMessageReceived(@NotNull PrivateMessageReceivedEvent event) {
-        if(!event.getAuthor().isBot()) {
+        if (!event.getAuthor().isBot()) {
         }
     }
 
     @Override
     public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent event) {
         TextChannel txt = event.getChannel();
-        Message msg = event.getMessage();
-        if(!msg.getAuthor().isBot()){
-            if(msg.getContentRaw().startsWith("!"))
-                txt.sendMessage("Nice One!!").complete();
+        String msg = event.getMessage().getContentRaw();
+        if (!event.getAuthor().isBot() && msg.startsWith(prefix)) {
+            if (msg.equalsIgnoreCase(prefix + "ping")) {
+                txt.sendTyping().complete();
+                txt.sendMessage("Pong!").complete();
+            }
         }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,15 @@
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.TextChannel;
-import net.dv8tion.jda.api.events.ReadyEvent;
-import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
-import net.dv8tion.jda.api.events.message.priv.PrivateMessageReceivedEvent;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
-import org.jetbrains.annotations.NotNull;
-
 import javax.security.auth.login.LoginException;
-import javax.swing.event.ChangeEvent;
-import java.io.*;
-import java.security.PrivilegedAction;
-import java.util.*;
-
-import static com.sun.tools.internal.xjc.model.CBuiltinLeafInfo.TOKEN;
 
 public class Main {
     static TextChannel textChannel;
 
     public static void main(String[] args)  throws LoginException, InterruptedException  {
-        JDABuilder JdaBuilder = JDABuilder.createDefault(TOKEN.toString());
+        JDABuilder JdaBuilder = JDABuilder.createDefault(new Token().toString());
         JdaBuilder.enableIntents(GatewayIntent.GUILD_MEMBERS);
         JdaBuilder.addEventListeners(new Help());
         JDA jda = JdaBuilder.build();
