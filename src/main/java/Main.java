5786e0494b26173df9605bd38938b6d7437c876f
Ziad Ak <ziadakgit@gmail.com>
2021-06-16 16:57:52 +0300

    added a bot class for bot initiating

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,8 @@
-import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.entities.TextChannel;
-import net.dv8tion.jda.api.requests.GatewayIntent;
-import javax.security.auth.login.LoginException;
 
 public class Main {
-    static TextChannel textChannel;
 
-    public static void main(String[] args)  throws LoginException, InterruptedException  {
-        JDABuilder JdaBuilder = JDABuilder.createDefault(Token.token);
-        JdaBuilder.enableIntents(GatewayIntent.GUILD_MEMBERS);
-        JdaBuilder.addEventListeners(new Help());
-        JDA jda = JdaBuilder.build();
-        Thread.sleep(2000);
-        Guild Server = jda.getGuilds().get(0);
-        textChannel = Server.getTextChannels().get(0);
-        textChannel.sendMessage("I'm ALIVE!").complete();
+    public static void main(String[] args) {
+        Bot bot = new Bot();
+        bot.StartBot();
     }
 }
