24f66a74f04deea13c7a5d20f3ce4fa3a353636b
Ziad AK <ziadakgit@gmail.com>
2021-06-19 17:47:29 +0300

    added selenium to the project with headless scraping
    added couple of functions using selenium that gives me shifts and something cool with instagram :)

--- src/main/java/Help.java
+++ src/main/java/Help.java
@@ -3,12 +3,26 @@ import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.priv.PrivateMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.apache.commons.io.FileUtils;
 import org.jetbrains.annotations.NotNull;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.LogManager;
 
 public class Help extends ListenerAdapter {
     String prefix = "!";
+    static final ChromeOptions CHROME_OPTIONS = new ChromeOptions();
+
+    public Help() {
+        System.setProperty(Token.CHROME_DRIVER,Token.CHROME_DRIVER_LOCATION);
+        CHROME_OPTIONS.addArguments("--headless");
+        CHROME_OPTIONS.addArguments("window-size=7680,4320");
+    }
 
     @Override
     public void onPrivateMessageReceived(@NotNull PrivateMessageReceivedEvent event) {
@@ -27,9 +41,41 @@ public class Help extends ListenerAdapter {
         Message message = event.getMessage();
         String message_content = message.getContentRaw();
 
-        //Checks if user that sent the message is not the bot so it wont loop itself and if it's starts with the prefix.
         if (!event.getAuthor().isBot() && message_content.startsWith(prefix))
         {
+            final WebDriver driver = new ChromeDriver(CHROME_OPTIONS);
+
+            if(message_content.startsWith(prefix + "getip")){
+                driver.get(Token.BASE_URL);
+                for (WebElement ip : driver.findElements(By.className("addrLeft"))) {
+                    txt.sendMessage("http://" + ip.getText()).complete();
+                }
+                driver.close();
+                driver.quit();
+            }
+
+            if (message_content.startsWith(prefix + "shifts")) {
+                driver.get(Token.BASE_URL2);
+                TakesScreenshot screenshot = (TakesScreenshot) driver;
+                File SrcFile = screenshot.getScreenshotAs(OutputType.FILE);
+                EmbedBuilder eb = new EmbedBuilder();
+                eb.setTitle("Your Shifts This Week");
+                eb.setColor(Color.GREEN);
+                eb.setThumbnail(author.getAvatarUrl());
+                txt.sendTyping().complete();
+                txt.sendMessage(eb.build()).addFile(SrcFile).queue();
+                driver.close();
+                driver.quit();
+            }
+
+            /** A function that shows the top picture of an instagram hashtag (Works only coupel of times a day)
+            if(message_content.startsWith(prefix + "instag")){
+                String tag = message_content.split("instag")[1].trim();
+                driver.get("https://www.instagram.com/explore/tags/" + tag);
+            }
+             **/
+
+
 
         }
     }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
+import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
+        Help help = new Help();
         Bot bot = new Bot();
         bot.StartBot();
     }
